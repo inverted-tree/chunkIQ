e3e9ca2f4ab563f863341ea2c2df814cdd25bb86
Lukas Schröder <lukasscr@proton.me>
2025-05-14 22:21:02 +0200

    Add unit tests to file and static chunkers
    The file-based and static chunkers now have unit tests.

--- src/chunker/fileChunker.rs
+++ src/chunker/fileChunker.rs
@@ -15,3 +15,42 @@ impl Chunker for FileChunker {
         (&mmap[..]).chunks(mmap.len())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::chunker::*;
+    use super::*;
+
+    use memmap2::Mmap;
+    use std::io::Write;
+    use tempfile::tempfile;
+
+    #[test]
+    fn testFileChunker() {
+        let mut file = tempfile().unwrap();
+        let data = b"Lorem ipsum dolor sit amet.";
+        file.write_all(data).unwrap();
+        let mmap = unsafe { Mmap::map(&file).unwrap() };
+
+        let chunker = FileChunker::new();
+        let mut chunks = chunker.chunk(&mmap);
+
+        assert_eq!(chunks.next(), Some(data.as_slice()));
+        assert!(chunks.next().is_none());
+    }
+
+    #[test]
+    fn testFileChunkerFactory() {
+        let mut file = tempfile().unwrap();
+        let data = b"hello world";
+        file.write_all(data).unwrap();
+        let mmap = unsafe { Mmap::map(&file).unwrap() };
+
+        let factory = ChunkFactory::new(ChunkerType::FILE, None);
+        let chunker = factory.createChunker();
+        let mut chunks = chunker.chunk(&mmap);
+
+        assert_eq!(chunks.next(), Some(data.as_slice()));
+        assert!(chunks.next().is_none());
+    }
+}
--- src/chunker/staticChunker.rs
+++ src/chunker/staticChunker.rs
@@ -17,3 +17,76 @@ impl Chunker for StaticChunker {
         mmap.chunks(self.size)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::chunker::*;
+    use super::*;
+
+    use memmap2::Mmap;
+    use std::io::Write;
+    use tempfile::tempfile;
+
+    #[test]
+    fn testStaticChunker() {
+        let types = vec![
+            ChunkerType::SC1K,
+            ChunkerType::SC2K,
+            ChunkerType::SC4K,
+            ChunkerType::SC8K,
+            ChunkerType::SC16K,
+            ChunkerType::SC32K,
+            ChunkerType::SC64K,
+        ];
+
+        for c in types {
+            let N = 64;
+
+            let mut file = tempfile().unwrap();
+            file.write_all(&vec![0u8; N * 1024]).unwrap();
+            let mmap = unsafe { Mmap::map(&file).unwrap() };
+
+            let chunker = StaticChunker::new(ChunkerType::getSize(&c));
+            let mut chunks = chunker.chunk(&mmap);
+
+            assert_eq!(chunks.len(), N * 1024 / ChunkerType::getSize(&c));
+            let expected = vec![0u8; ChunkerType::getSize(&c)];
+            for chunk in chunks.by_ref() {
+                assert_eq!(chunk, expected.as_slice());
+            }
+            assert!(chunks.next().is_none());
+        }
+    }
+
+    #[test]
+    fn testStaticChunkerFactory() {
+        let types = vec![
+            ChunkerType::SC1K,
+            ChunkerType::SC2K,
+            ChunkerType::SC4K,
+            ChunkerType::SC8K,
+            ChunkerType::SC16K,
+            ChunkerType::SC32K,
+            ChunkerType::SC64K,
+        ];
+
+        for c in types {
+            let N = 64;
+
+            let mut file = tempfile().unwrap();
+            file.write_all(&vec![0u8; N * 1024]).unwrap();
+            let mmap = unsafe { Mmap::map(&file).unwrap() };
+
+            let factory = ChunkFactory::new(c, None);
+            let chunker = factory.createChunker();
+            let mut chunks = chunker.chunk(&mmap);
+
+            assert_eq!(chunks.len(), N * 1024 / ChunkerType::getSize(&c));
+            let expected = vec![0u8; ChunkerType::getSize(&c)];
+            for chunk in chunks.by_ref() {
+                assert_eq!(chunk, expected.as_slice());
+            }
+            assert!(chunks.next().is_none());
+        }
+    }
+}
